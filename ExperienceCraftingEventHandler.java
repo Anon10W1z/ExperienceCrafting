f0ef7c99e89562f8f4231b3d18fa79adb73aa0b8
Anon10W1z <cyrus.bugwadia@gmail.com>
2015-07-31 11:00:31 -0700

    Update ExperienceCraftingEventHandler.java

--- ExperienceCraftingEventHandler.java
+++ ExperienceCraftingEventHandler.java
@@ -3,26 +3,33 @@ package io.github.anon10w1z.expcrafting;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 
+/**
+ * The Experience Crafting event handler
+ */
 public class ExperienceCraftingEventHandler {
+	/**
+	 * Ensures the max experience level for every player is kept in sync
+	 * @param event The LivingUpdateEvent
+	 */
 	@SubscribeEvent
 	public void onLivingUpdate(LivingUpdateEvent event) {
 		if (event.entityLiving instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer) event.entityLiving;
-			if (player.getExtendedProperties("EXPERIENCE_CRAFTING_PROPERTIES") == null)
-				player.registerExtendedProperties("EXPERIENCE_CRAFTING_PROPERTIES", new ExperienceCraftingPlayerProperties());
 			ExperienceCraftingPlayerProperties.updateMaxExperienceLevel(player);
 		}
 	}
 	
+	/**
+	 * Copies the max experience level for a player upon death/dimension change
+	 * @param event The player clone event
+	 */
 	@SubscribeEvent
 	public void onPlayerClone(PlayerEvent.Clone event) {
 		ExperienceCraftingPlayerProperties oldPlayerProperties = (ExperienceCraftingPlayerProperties) event.original.getExtendedProperties("EXPERIENCE_CRAFTING_PROPERTIES");
-		if (event.entityPlayer.getExtendedProperties("EXPERIENCE_CRAFTING_PROPERTIES") == null)
-			event.entityPlayer.registerExtendedProperties("EXPERIENCE_CRAFTING_PROPERTIES", new ExperienceCraftingPlayerProperties());
+		if (event.entityPlayer.getExtendedProperties(ExperienceCraftingPlayerProperties.PROPERTIES_NAME) == null)
+			event.entityPlayer.registerExtendedProperties(ExperienceCraftingPlayerProperties.PROPERTIES_NAME, new ExperienceCraftingPlayerProperties());
 		ExperienceCraftingPlayerProperties newPlayerProperties = (ExperienceCraftingPlayerProperties) event.entityPlayer.getExtendedProperties("EXPERIENCE_CRAFTING_PROPERTIES");
 		newPlayerProperties.maxExperienceLevel = oldPlayerProperties.maxExperienceLevel;
 	}
