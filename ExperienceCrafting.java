971e350cac5e31d67cd22a0d84a6a875f07c9acd
Anon10W1z <cyrus.bugwadia@gmail.com>
2015-07-31 11:00:11 -0700

    Update ExperienceCrafting.java

--- ExperienceCrafting.java
+++ ExperienceCrafting.java
@@ -5,16 +5,22 @@ import java.util.List;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.oredict.RecipeSorter;
 import net.minecraftforge.oredict.RecipeSorter.Category;
 
+/**
+ * Experience Crafting main mod file
+ */
 @Mod(modid = "expcrafting", name = "Experience Crafting", version = "1.0", dependencies = "after:*")
 public class ExperienceCrafting {
+	/**
+	 * Initializes Experience Crafting
+	 * @param event The FMLInitializationEvent
+	 */
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
 		RecipeSorter.register("wrapped_recipe", WrappedRecipe.class, Category.SHAPED, "");
@@ -22,5 +28,6 @@ public class ExperienceCrafting {
 		for (int i = 0; i < recipes.size(); ++i)
 			recipes.set(i, new WrappedRecipe(recipes.get(i)));
 		MinecraftForge.EVENT_BUS.register(new ExperienceCraftingEventHandler());
+		FMLCommonHandler.instance().bus().register(new ExperienceCraftingEventHandler());
 	}
 }
